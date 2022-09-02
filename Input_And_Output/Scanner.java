2a5284474d9315c5ec1720f257a03022e0cc5fbc
Warren Spalding <w.spalding.toc@gmail.com>
2022-09-02 15:56:56 +0100

    update Scanner.java
    added comments

--- Input_And_Output/Scanner.java
+++ Input_And_Output/Scanner.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
+// Scanner class that reads user's input.
 public class Scanner {
   public static void main( String[] args ) {
     
+    // declares the Scanner
     Scanner input = new Scanner(System.in);
     
+    // question
     System.out.print("What is your name?: ");
+    // awaits user input
     String userName = input.next();
+    // printed response including the user input
     System.out.printf("Hello %s! It's nice to meet you.\n", userName);
 
   }
