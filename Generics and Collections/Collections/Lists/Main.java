0adc464126a78b84a049939279351775231a2907
Warren Spalding <w.spalding.toc@gmail.com>
2022-09-07 18:37:45 +0100

    added Lists

---
+++ Generics and Collections/Collections/Lists/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+
+    List<String> stringList = new ArrayList<>(); // Empty `list`
+    stringList.add("Hello");
+    stringList.add("World");
+    stringList.add("!");
+
+    for (String element: stringList) {
+      System.out.println(element); // Prints `Hello, World, !`
+    }
+
+  }
+}
