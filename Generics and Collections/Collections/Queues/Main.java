6b99121148bb40d0052c84a9d7befe8cc4aa8331
Warren Spalding <w.spalding.toc@gmail.com>
2022-09-07 21:02:40 +0100

    update Queues/Main.java
    added comments

--- Generics and Collections/Collections/Queues/Main.java
+++ Generics and Collections/Collections/Queues/Main.java
@@ -2,35 +2,43 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    Queue<String> line = new LinkedList<>(); // Empty LinkedList
-    line.add("Mike"); // ["Mike"]
-    line.add("Isabel"); // ["Mike", "Isabel"]
-    line.add("Jenny"); // ["Mike", "Isabel", "Jenny"]
+    List<Integer> myInts = new ArrayList<>(); // Initializes Empty ArrayList
 
-    for(String name: line) {
-      System.out.println(name); // Iterate each name of the queue
+    myInts.add(9); // -> 9 <-
+    myInts.add(13); // -> 9, 13 <-
+    myInts.add(2); // -> 9, 13, 2 <-
+    myInts.add(1); // -> 9, 13, 2, 1 <-
+    myInts.add(11); // -> 9, 13, 2, 1, 11 <-
+    myInts.add(39); // -> 9, 13, 2, 1, 11, 39 <-
+    myInts.add(78); // -> 9, 13, 2, 1, 11, 39, 78 <-
+    myInts.add(4); // -> 9, 13, 2, 1, 11, 39, 78, 4 <-
+
+    Deque<Integer> result = separateInts(myInts);
+    for(Integer i: result) {
+      System.out.println(i);
     }
-
-    processAlphabetically(line); // Calls the Alphabetical Linked List
-    // Processing: Isabel
-    // Processing: Jenny
-    // Processing: Mike
-
   }
-  public static void processAlphabetically(Queue<String> queue) {
+  public static Deque<Integer> separateInts(List<Integer> integers) {
 
-    Queue<String> alphabeticalQueue = new PriorityQueue<>(); // Initalize Priority Queue
+    Deque<Integer> separatedDeque = new ArrayDeque<>(); // Initialize Empty ArrayDeque
 
-    for (String name: queue) {
-      alphabeticalQueue.offer(name); // For loop that iterates all elements of the queue
-    }
-    while (alphabeticalQueue.peek() != null) {
-      String headElement = alphabeticalQueue.poll(); // While loop that Iterates the head element of the queue and removes it
-      System.out.println("Processing: " + headElement); // prints processing message of the head element removed from the queue
-    }
-
-  }
+    //for enhanced loop iterates the integers that are even or odd
+    for (Integer myInt: integers) {
+      if (myInt % 2 == 0) {
+        separatedDeque.addFirst(myInt); // adds even element to the front of the Deque
+      } else {
+        separatedDeque.addLast(myInt); // adds odd element to the end of the Deque
+      }
   
+    }
+    return separatedDeque; // returns the -> even, odd <- deque
+    // Result: -> 4, 78, 2, 9, 13, 1, 11, 39 <-
+  }
 }
